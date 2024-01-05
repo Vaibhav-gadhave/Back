49e1568ae47e0dfc13f4386274e3e7f819a07870
Vaibhav-gadhave <vvgadhave@gmail.com>
2024-01-05 15:11:36 +0530

    unwanted files moved

---
+++ scripts-learned/arithmatic-operations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Created date :  2024-01-05 02:32:43 PM
+#Modified date : 
+#Author: Vaibhav Gadhave
+#Start 
+
+read -p "Enter one number: " num1
+read -p  "Enter another number: " num2
+
+echo -e "Addition of $num1 and $num2 is $(($num1 + $num2)) "
+echo -e "Substraction of $num1 and $num2 is $(($num1 - $num2))"
+echo -e "Division of $num1 and $num2 is $(($num1 / $num2))"
+echo -e "Multiplication of $num1 and $num2 is $(($num1 * $num2))"
+#End
