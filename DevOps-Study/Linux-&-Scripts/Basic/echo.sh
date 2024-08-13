8c053034550e54f2b3c50d8c0ff4d03b9c4f3c98
Vaibhav Gadhave <vvgadhave@gmail.com>
2024-08-13 12:37:16 +0530

    Interview questions updated

--- DevOps-Study/Linux-&-Scripts/Basic/echo.sh
+++ DevOps-Study/Linux-&-Scripts/Basic/echo.sh
@@ -4,36 +4,50 @@
 #Author: Vaibhav Gadhave
 #Start 
 
+echo "-------------------------------------------------------------------"
 echo "########### single quotes #############"
 echo -e '$(ls)'
 
+echo "-------------------------------------------------------------------"
 echo "########### double quotes #############"
 echo -e "$(ls)"
 
+echo "-------------------------------------------------------------------"
 echo "########### no quotes #############"
 echo -e $(ls)
 
+echo "-------------------------------------------------------------------"
 echo "########### Next line #############"
 echo -e "i am Devops Engineer\nI study Everyday"
 
+echo "-------------------------------------------------------------------"
 echo "########### tab #############"
 echo -e "i am Devops Engineer\tI study Everyday"
 
+echo "-------------------------------------------------------------------"
 echo "########### vertical tab #############"
 echo -e "i am Devops Engineer\vI study Everyday"
 
+echo "-------------------------------------------------------------------"
 echo "########### backslash #############"
 echo -e "i am Devops Engineer\bI study Everyday"
 
+echo "-------------------------------------------------------------------"
 echo "########### alert sound #############"
 echo -e "i am Devops Engineer I study Everyday \a"
 
+echo "-------------------------------------------------------------------"
 echo "########### color #############"
 echo -e "\033[104mi am Devops Engineer I study Everyday\033[0m"
 
+echo "-------------------------------------------------------------------"
+echo "########### Quotes  #############"
+echo -e "i am Devops Engineer I study Everyday, my favourite number is \$3"
+
+echo "-------------------------------------------------------------------"
+echo "   For more info please do {man echo}   "
+
+echo "-------------------------------------------------------------------"
 echo "########### don not output tailing new line #############"
 echo -n "i am Devops Engineer I study Everyday"
-
-echo "For more info please do {man echo} "
-
 #End
