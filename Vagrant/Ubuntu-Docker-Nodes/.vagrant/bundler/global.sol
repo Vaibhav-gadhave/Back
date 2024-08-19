7f89472e839f531c4a4f21c09c30a7fdd8c24798
Vaibhav-gadhave <vvgadhave@gmail.com>
2024-08-19 14:16:49 +0530

    unwanted Vagrant file deleted

--- Vagrant/Ubuntu-Docker-Nodes/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"36c5cda303ef35fb837811f94a090d44cb2affbe62a7e1f4c4bc89c62a1cd413","vagrant_version":"2.4.1"}
