7d6f11553854d81204f7129f35f5ff29f126f6ef
Vaibhav-gadhave <vvgadhave@gmail.com>
2025-01-02 11:41:39 +0530

    Python learning started

---
+++ DevOps-Study/Python/Basic/basic.py
@@ -0,0 +1,24 @@
+print("Add 2 numbers")
+a=2 
+b=3
+c=a+b
+print(c)
+
+print("Substract 2 numbers")
+a=6 
+b=3
+c=a-b
+print(c)
+
+print("Multiply 2 numbers")
+a=2 
+b=3
+c=a*b
+print(c)
+
+print("Divide 2 numbers")
+a=2 
+b=3
+c=a / b
+print(c)
+
