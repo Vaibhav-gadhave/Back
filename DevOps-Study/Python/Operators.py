0ccdb83386183f0f23b04d60fbedd75da042fed0
Vaibhav-gadhave <vvgadhave@gmail.com>
2025-01-26 00:02:32 +0530

    PXE updated for rocky linux 9.5

---
+++ DevOps-Study/Python/Operators.py
@@ -0,0 +1,49 @@
+print("Arithmetic Operators")
+x=2
+y=3
+x=x+2
+print(x)
+
+x+=2
+print(x)
+
+x*=2
+print(x)
+
+print("Assign multiple variables in sinle line")
+a,b=3,5
+print(a)
+print(b)
+
+print("Relation Operators")
+print(a < b)
+print(a == b)
+print(a != b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+print("Logical Operators")
+print(a > b and a < b)
+print(a > b or a < b)
+print(a > b and a == b)
+print(a > b or a == b)
+print(not(a > b))
+print(not(a < b))
+print(not(a == b))
+print(not(a != b))
+
+print("Binary Operators")
+print(bin(25))
+print(0b0101)
+
+print(oct(25))
+print(0o32)
+
+print(hex(25))
+print(0xa)
+
+print("Bitwise Operators")
+
+
+
